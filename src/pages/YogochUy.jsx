3032bdef6de83124c5b376d0cf857e461c490cec
Abduazizbek <abduazizbekqobulov4@gmail.com>
2024-08-05 12:56:18 +0300

    Add netlify toml

--- src/pages/YogochUy.jsx
+++ src/pages/YogochUy.jsx
@@ -7,6 +7,7 @@ const YogochUy = () => {
   return (
     <PageWrapper>
     <div className='w-full max-w-base pt-12 pb-14 my-12 mx-auto'>
+      
       <div className="flex items-center mb-5">
         <Link to={'/'} className="text-[#6cd16c] text-[16px]"> <u>Главная</u></Link>
         <p className="text-[#969494] text-[16px]"><span className='mx-1'>/</span>Бревенчатые дома</p>
